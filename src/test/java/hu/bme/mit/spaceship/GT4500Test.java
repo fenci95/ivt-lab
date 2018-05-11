9acaa600791a16ffdf31b78b10a51007a3e27ecc
meres <Fenes ron>
2018-05-11 00:05:23 -0700

    Osszes integracio megvalositas feladat

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,16 +9,21 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPrimary;
+  private TorpedoStore mockSecondary;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+    mockPrimary = mock(TorpedoStore.class);
+    mockSecondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimary, mockSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockPrimary.fire(1)).thenReturn(true);
+    when(mockSecondary.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
@@ -29,12 +34,36 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockPrimary.fire(mockPrimary.getTorpedoCount())).thenReturn(true);
+    when(mockSecondary.fire(mockSecondary.getTorpedoCount())).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success2(){
+    // Arrange
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockPrimary).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_All_Success2(){
+    // Arrange
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockPrimary).fire(mockPrimary.getTorpedoCount());
+    verify(mockSecondary).fire(mockSecondary.getTorpedoCount());
   }
 
 }
